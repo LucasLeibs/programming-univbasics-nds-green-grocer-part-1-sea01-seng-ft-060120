5c4bd2d5b58068f290f35fff088228ae64094afc
Lucas Leiberman <dr.lucasleibs@gmail.com>
2020-05-18 18:48:18 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -31,7 +31,8 @@ def consolidate_cart(cart)
   i = 0 
   while (find_item_by_name_in_collection(cart[i][:item], new_cart) == nil )
   cart[i][:count] = 1 
-  new_cart.push(cart[i])
+  new_cart.push(cart[i]
+else
   
   # Consult README for inputs and outputs
   #

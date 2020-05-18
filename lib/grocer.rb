eded3ed4ccf2c0b2758a745b8e43552f971305de
Lucas Leiberman <dr.lucasleibs@gmail.com>
2020-05-18 18:34:33 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,11 +1,35 @@
+def increment_count_of_item(cart, item_name)
+  i = 0 
+  while i < cart.length do 
+    if ( cart[i][:item] == item_name)
+      cart[i][:count] += 1
+    end
+    i += 1 
+  end
+  cart
+  end
+
+
 def find_item_by_name_in_collection(name, collection)
+   count = 0
+  while count < collection.length do
+    if ( collection[count][:item] == name )
+      return collection[count]
+    end
+    count += 1
+  end
+  nil
+end
   # Implement me first!
   #
   # Consult README for inputs and outputs
 
-end
+
 
 def consolidate_cart(cart)
+  
+  
+  
   # Consult README for inputs and outputs
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
